840199f6710e37f398ef78abf03bed36076bfb58
Karim Boucher <boucherkarim@gmail.com>
2020-09-09 11:57:01 -0400

    Encode data as 'utf-8'

--- electrum/plugins/cosigner_pool/qt.py
+++ electrum/plugins/cosigner_pool/qt.py
@@ -94,7 +94,7 @@ class Listener(util.DaemonThread):
                     if lock:
                         continue
                     data = server.get(self.wallet_hash)
-                    sha1_data = hashlib.sha1(data).hexdigest() # sha1 of the data (to check for udpates)
+                    sha1_data = hashlib.sha1(data.encode('utf-8')).hexdigest() # sha1 of the data (to check for udpates)
                 except Exception as e:
                     self.logger.info("cannot contact cosigner pool")
                     time.sleep(30)
