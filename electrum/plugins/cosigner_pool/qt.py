8af6f199384a31ceb25c14f552f07a1891b61220
Karim Boucher <boucherkarim@gmail.com>
2020-09-11 10:31:09 -0400

    Potential fix for TxStatus freezing on Mac

--- electrum/plugins/cosigner_pool/qt.py
+++ electrum/plugins/cosigner_pool/qt.py
@@ -211,7 +211,8 @@ class Plugin(BasePlugin):
 
 
     def tx_status_dialog(self, window):
-        d = WindowModalDialog(window, _("Transaction Status"))
+        d = QDialog(window)
+        d.setWindowTitle(_("Transaction Status"))
 
         d.setMinimumSize(600, 300)
 
@@ -269,8 +270,8 @@ class Plugin(BasePlugin):
         vbox.addSpacing(13)
         vbox.addLayout(Buttons(CloseButton(d)))
         
-        if not d.exec_():
-            return
+        d.setModal(True)
+        d.show()
 
     def is_available(self):
         return True
